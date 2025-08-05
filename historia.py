a4e976a868c2b108d00569dd0422c50cb5630926
Asielpup <monkybravo22@gmail.com>
2025-08-04 20:21:41 -0600

    Parte 3 de la historia

--- historia.py
+++ historia.py
@@ -1,3 +1,3 @@
 print("Parte 1: Juan salió de su casa muy temprano.")
 print("Parte 2: Se encontró con un dragón en la entrada del bosque.")  # Integrante 2
-print("Parte 3: Juan escapó .") #Alumno 3
+print("Parte 3: Juan juega futbol con el dragon.")  # Alumno 3
